9b371f973f0cca829d7d753d1fe46748236814b0
mikejuyoon <mikejuyoon@gmail.com>
2014-10-31 17:39:27 -0700

    Add calling API for gas stations pt1

--- app/src/main/java/com/mobico/rcart/GasStationsList.java
+++ app/src/main/java/com/mobico/rcart/GasStationsList.java
@@ -3,23 +3,140 @@ package com.mobico.rcart;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class GasStationsList extends Activity {
 
     LinearLayout gasStationList;
+    JSONObject gasJson;
+    JSONArray gasArray;
+
+    EditText etResponse;
+    TextView tvIsConnected;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gas_stations_list);
+
+        // get reference to the views
+        etResponse = (EditText) findViewById(R.id.etResponse);
+        tvIsConnected = (TextView) findViewById(R.id.tvIsConnected);
+
+        // check if you are connected or not
+
+        if(isConnected()){
+            tvIsConnected.setBackgroundColor(0xFF00CC00);
+            tvIsConnected.setText("You are conncted");
+        }
+        else{
+            tvIsConnected.setText("You are NOT conncted");
+        }
+
+        // call AsynTask to perform network operation on separate thread
+        new HttpAsyncTask().execute("https://mobibuddy.herokuapp.com/nearby_gas.json?lat=33.971&long=-117.35&dist=1");
+    }
+
+    public static String GET(String url){
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // make GET request to the given URL
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return GET(urls[0]);
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Received!", Toast.LENGTH_LONG).show();
+            try{
+                gasJson = new JSONObject(result);
+                gasArray = gasJson.getJSONArray("stations");
+                //etResponse.setText(json.toString(1));
+            }
+            catch(JSONException e){
+                //do nothing
+            }
+        }
     }
 
 
@@ -42,17 +159,37 @@ public class GasStationsList extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
     public void fillList(View view){
-        for(int i = 0 ; i < 5 ; i++){
-            pushListItemToLayoutView();
+        gasStationList = (LinearLayout) findViewById(R.id.gasStationList);
+        for(int i = 0 ; i < gasArray.length() ; i++){
+            pushListItemToLayoutView(i);
         }
     }
 
-    private void pushListItemToLayoutView(){
-        gasStationList = (LinearLayout) findViewById(R.id.gasStationList);
+    private void pushListItemToLayoutView(int i){
+        //gasStationList = (LinearLayout) findViewById(R.id.gasStationList);
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
         View listItem = inflater.inflate(R.layout.gas_station_row, null);
+
+        //View newStockRow = inflater.inflate(R.layout.stock_quote_row, null);
+
+        // Create the TextView for the ScrollView Row
+        TextView newStockTextView = (TextView) listItem.findViewById(R.id.stockSymbolTextView);
+
+        // Add the stock symbol to the TextView
+        newStockTextView.setText(stock);
+
+        Button stockQuoteButton = (Button) newStockRow.findViewById(R.id.stockQuoteButton);
+        stockQuoteButton.setOnClickListener(getStockActivityListener);
+
+        Button quoteFromWebButton = (Button) newStockRow.findViewById(R.id.quoteFromWebButton);
+        quoteFromWebButton.setOnClickListener(getStockFromWebsiteListener);
+
+        // Add the new components for the stock to the TableLayout
+        stockTableScrollView.addView(newStockRow, arrayIndex);
 
         //TextView listNameTV = (TextView) listItem.findViewById(R.id.listNameTV);
         //listNameTV.setText(s);
