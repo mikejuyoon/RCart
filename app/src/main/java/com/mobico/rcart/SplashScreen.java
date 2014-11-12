a857489e7a548f778370bf3658959714f35a0b16
mikejuyoon <mikejuyoon@gmail.com>
2014-11-11 22:15:17 -0800

    Add essential build files

--- app/src/main/java/com/mobico/rcart/SplashScreen.java
+++ app/src/main/java/com/mobico/rcart/SplashScreen.java
@@ -2,23 +2,23 @@ package com.mobico.rcart;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-
+import android.widget.TextView;
 
 public class SplashScreen extends Activity {
 
-    Button gasListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        gasListButton = (Button) findViewById(R.id.gas_list_btn);
     }
 
 
@@ -41,6 +41,7 @@ public class SplashScreen extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
     public void openLoginActivity(View view){
         Intent i = new Intent(SplashScreen.this, Login.class);
         startActivity(i);
@@ -52,4 +53,5 @@ public class SplashScreen extends Activity {
         startActivity(i);
         finish();
     }
+
 }
