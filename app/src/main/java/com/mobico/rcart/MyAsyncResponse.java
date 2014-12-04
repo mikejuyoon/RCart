fc09a445ab7df9220ff5efd9a53322bef8032445
Justin Khoo <jkhoo002@ucr.edu>
2014-12-03 16:43:13 -0800

    clean code

--- app/src/main/java/com/mobico/rcart/MyAsyncResponse.java
+++ app/src/main/java/com/mobico/rcart/MyAsyncResponse.java
@@ -1,12 +1,6 @@
 package com.mobico.rcart;
 
-/**
-<<<<<<< HEAD
- * Created by Micheal on 12/3/14.
-=======
- * Created by Michael on 12/3/2014.
->>>>>>> 95d8243b46851dd70e497ccccf60f0537a846f32
- */
+
 public interface MyAsyncResponse {
     void processFinish(String output);
 }
