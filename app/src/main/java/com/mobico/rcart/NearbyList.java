c9eb582a49fe199f090a406a2a7e0596b214925d
mikejuyoon <mikejuyoon@gmail.com>
2014-12-03 17:48:35 -0800

    Add Image loading

--- app/src/main/java/com/mobico/rcart/NearbyList.java
+++ app/src/main/java/com/mobico/rcart/NearbyList.java
@@ -3,12 +3,17 @@ package com.mobico.rcart;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,6 +25,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -82,7 +88,8 @@ public class NearbyList extends Activity implements MyAsyncResponse{
             }
         });
 
-        invalidEntryAlert("name: " + name + "\nimageUrl: " + imageUrl + "\nprice: " + price + "\ncategory: " + category);
+        new DownloadImageTask((ImageView) findViewById(R.id.product_thumbnail)).execute(imageUrl);
+        //invalidEntryAlert("name: " + name + "\nimageUrl: " + imageUrl + "\nprice: " + price + "\ncategory: " + category);
     }
 
     @Override
@@ -154,7 +161,8 @@ public class NearbyList extends Activity implements MyAsyncResponse{
 
                 HashMap<String, String> storeInfo = new HashMap<String, String>();
                 storeInfo.put("store_name", resultsJson.getJSONObject(i).getString("name"));
-                storeInfo.put("store_price", String.valueOf(newPrice));
+                //storeInfo.put("store_price", String.valueOf(newPrice));
+                storeInfo.put("store_price", String.format("%.2f",newPrice));
                 storeInfo.put("store_distance", "1.5");
                 storeInfo.put("lati", String.valueOf(latitude));
                 storeInfo.put("longi", String.valueOf(longitude));
@@ -175,4 +183,29 @@ public class NearbyList extends Activity implements MyAsyncResponse{
         }catch(Exception e){}
         updateLocationsList();
     }
+
+    // private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    //     ImageView bmImage;
+
+    //     public DownloadImageTask(ImageView bmImage) {
+    //         this.bmImage = bmImage;
+    //     }
+
+    //     protected Bitmap doInBackground(String... urls) {
+    //         String urldisplay = urls[0];
+    //         Bitmap mIcon11 = null;
+    //         try {
+    //             InputStream in = new java.net.URL(urldisplay).openStream();
+    //             mIcon11 = BitmapFactory.decodeStream(in);
+    //         } catch (Exception e) {
+    //             Log.e("Error", e.getMessage());
+    //             e.printStackTrace();
+    //         }
+    //         return mIcon11;
+    //     }
+
+    //     protected void onPostExecute(Bitmap result) {
+    //         bmImage.setImageBitmap(result);
+    //     }
+    // }
 }
